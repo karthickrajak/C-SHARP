3c47c9da3fffbef8320104f2266ff162f9961a8f
Karthick rajakannathasan <77183053+karthickrajak@users.noreply.github.com>
2024-09-17 09:21:08 +0530

    Add files via upload

---
+++ Swapping/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace swap2no
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, k;
+            Console.WriteLine("Enter the two numbers");
+            i = int.Parse(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
+            Console.WriteLine("Before swapping i={0} and k={1}", i, k);
+            i = i ^ k;
+            k = i ^ k;
+            i = i ^ k;
+            Console.Write("After swapping i={0} and k={1}", i, k);
+            Console.ReadLine();
+        }
+    }
+}
