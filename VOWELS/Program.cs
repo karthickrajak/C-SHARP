9403d200cc107d4878179f3b291043ad73dba262
Karthick rajakannathasan <77183053+karthickrajak@users.noreply.github.com>
2024-09-17 09:19:16 +0530

    Add files via upload

---
+++ VOWELS/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace vowels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Vowels :");
+            char input = char.ToLower(Convert.ToChar(Console.ReadLine()));
+            if(input=='a' || input=='e' || input=='i' || input=='o' || input=='u')
+            {
+                Console.Write("The given word is Vowel");
+            }
+            else
+            {
+                Console.Write("This is not an Vowels");
+                Console.ReadLine();
+            }
+        }
+    }
+}
