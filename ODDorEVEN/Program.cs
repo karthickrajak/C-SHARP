5311455626dfd3e36a832b08c132ff2517f58c65
Karthick rajakannathasan <77183053+karthickrajak@users.noreply.github.com>
2024-09-17 09:18:10 +0530

    Add files via upload

---
+++ ODDorEVEN/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_odd_or_even
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k;
+            Console.Write("Enter the number-->");
+            k = int.Parse(Console.ReadLine());
+            if (k % 2 == 0)
+            { Console.Write("This is an Even numbar");
+                Console.ReadLine();
+            
+            }
+            else
+            { Console.Write("This is an ODD number");
+                Console.ReadLine();
+            }
+        }
+    }
+}
