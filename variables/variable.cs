676f22dbb3d90b64c946c4fac225b95359fd33ae
Karthick rajakannathasan <77183053+karthickrajak@users.noreply.github.com>
2024-09-17 09:12:19 +0530

    Add files via upload

--- variables/variable.cs
+++ variables/variable.cs
@@ -1,1 +1,21 @@
+﻿namespace variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable declaration
+            string user = "Accord Info Matrix";
+            short age = 20;
+            float score = 12.2f;
+            bool result= score >= 50;
 
+            //Output statements
+
+            Console.WriteLine("Welcome"+user);
+            Console.WriteLine("Your Age"+age);
+            Console.WriteLine("Score"+score);
+            Console.WriteLine("Result"+result);
+        }
+    }
+}
